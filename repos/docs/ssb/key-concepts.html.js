e2cc9f91c394a6e282e035a885ac1af2ceb395eb
Paul Frazee <pfrazee@gmail.com>
2016-01-15 21:09:40 -0600

    remove docs/ssb/key-concepts

--- repos/docs/ssb/key-concepts.html.js
+++
@@ -1,8 +0,0 @@
-module.exports = function* (gen) {
-  yield gen.partials.page({
-    title: 'Key Concepts - Scuttlebot - SSBC',
-    section: '/scuttlebot',
-    path: '/docs/ssb/key-concepts.html',
-    content: gen.readMD('ssb/key-concepts.md')
-  })
-}
