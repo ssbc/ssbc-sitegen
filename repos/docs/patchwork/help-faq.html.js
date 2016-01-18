4e41a729d4150b53aa91b9e58f327fcecfaca89b
Paul Frazee <pfrazee@gmail.com>
2016-01-18 14:13:24 -0600

    remove repos/docs/patchwork/*

--- repos/docs/patchwork/help-faq.html.js
+++
@@ -1,8 +0,0 @@
-module.exports = function* (gen) {
-  yield gen.partials.page({
-    title: 'Help / FAQ - Patchwork - SSBC',
-    section: '/patchwork',
-    path: '/docs/patchwork/help-faq.html',
-    content: gen.readMD('patchwork/help-faq.md')
-  })
-}
--- repos/docs/patchwork/install.html.js
+++
@@ -1,8 +0,0 @@
-module.exports = function* (gen) {
-  yield gen.partials.page({
-    title: 'Install - Patchwork - SSBC',
-    section: '/patchwork',
-    path: '/docs/patchwork/install.html',
-    content: gen.readMD('patchwork/install.md')
-  })
-}
